e3703668a4b17749d27dd85ec057b232f4a61ebc
KamaelLeRandom <kamaellerandom@gmail.com>
2026-01-15 14:20:15 +0100

    feat: add map on main activity

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,6 +38,8 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation("org.osmdroid:osmdroid-android:6.0.2")
+    implementation("com.google.android.gms:play-services-location:21.0.1")
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
